d9f7cce3a7d95d3297f19e2cb787551338cc8a3d
Sarenis <80856920+sarenis@users.noreply.github.com>
2024-08-05 13:48:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ function initializeMap(position) {
     map = L.map('map').setView(userLocation, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        minZoom: 30
+        maxZoom: 19
     }).addTo(map);
 
     const userIcon = L.divIcon({
