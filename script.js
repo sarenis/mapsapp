a56a6351a382b94df5d94b918a75da5f35db5244
sarenis <lolikibick@gmail.com>
2024-08-05 11:46:31 +0300

    second

---
+++ script.js
@@ -0,0 +1,46 @@
+let map;
+let userMarker;
+
+function initializeMap(position) {
+    const { latitude, longitude } = position.coords;
+    const userLocation = [latitude, longitude];
+
+    map = L.map('map').setView(userLocation, 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19
+    }).addTo(map);
+
+    userMarker = L.marker(userLocation).addTo(map)
+        .bindPopup('Ти тут!')
+        .openPopup();
+
+    // Налаштування постійного оновлення місцезнаходження
+    navigator.geolocation.watchPosition(updateUserLocation, handleGeolocationError, {
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+        timeout: 5000
+    });
+}
+
+function updateUserLocation(position) {
+    const { latitude, longitude } = position.coords;
+    const userLocation = [latitude, longitude];
+
+    // Оновлення положення маркера
+    userMarker.setLatLng(userLocation);
+
+    // Перевірка наближення до якихось точок, якщо потрібно
+    // checkProximity();
+    console.log('Місцезнаходження оновлено:', userLocation);
+}
+
+function handleGeolocationError(error) {
+    alert("Не вдалося отримати геопозицію: " + error.message);
+}
+
+// Отримання початкового місцезнаходження та ініціалізація карти
+navigator.geolocation.getCurrentPosition(initializeMap, handleGeolocationError, {
+    enableHighAccuracy: true,
+    timeout: 5000
+});
