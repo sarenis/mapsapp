84612232faac0366b4681c54c397cc5ed63e195a
Sarenis <80856920+sarenis@users.noreply.github.com>
2024-08-05 16:20:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCzxLE0dovqEppLKlCk_xipfQsq0m_n2hs",
-  authDomain: "sarenmap.firebaseapp.com",
-  databaseURL: "https://sarenmap-default-rtdb.firebaseio.com",
-  projectId: "sarenmap",
-  storageBucket: "sarenmap.appspot.com",
-  messagingSenderId: "472187095328",
-  appId: "1:472187095328:web:e5b067bdc1a5c61007ef45",
-  measurementId: "G-ZPCR33ZHWY"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
-
 let map;
 let userMarker;
-const otherUsersMarkers = {};
 
 function initializeMap(position) {
     const { latitude, longitude } = position.coords;
@@ -31,24 +15,19 @@ function initializeMap(position) {
         html: `<div class="icon-container">
                   <img src="saren.jpg" width="35" height="35">
                </div>`,
-        iconSize: [39, 39],
-        iconAnchor: [27, 27],
-        popupAnchor: [-8, -27]
+        iconSize: [39, 39], // Розмір іконки разом з рамкою і відступом
+        iconAnchor: [27, 27], // Точка прив'язки іконки (центр низу)
+        popupAnchor: [-8, -27] // Точка прив'язки попапа
     });
 
-    userMarker = L.marker(userLocation, { icon: userIcon }).addTo(map);
+    userMarker = L.marker(userLocation, { icon: userIcon }).addTo(map)
     
     userMarker.on('click', function () {
         map.flyTo(userLocation, map.getMaxZoom());
     });
+    
 
-    // Post user's location to Firebase
-    postUserLocation(position);
-
-    // Listen for location updates of other users
-    listenForOtherUsers();
-
-    // Start watching user's location
+    // Налаштування постійного оновлення місцезнаходження
     navigator.geolocation.watchPosition(updateUserLocation, handleGeolocationError, {
         enableHighAccuracy: true,
         maximumAge: 1000,
@@ -56,60 +35,23 @@ function initializeMap(position) {
     });
 }
 
-function postUserLocation(position) {
-    const { latitude, longitude } = position.coords;
-    const userId = "user1"; // Change this to unique ID for each user
-
-    database.ref('users/' + userId).set({
-        latitude: latitude,
-        longitude: longitude
-    });
-}
-
 function updateUserLocation(position) {
     const { latitude, longitude } = position.coords;
     const userLocation = [latitude, longitude];
 
+    // Оновлення положення маркера без зміни виду карти
     userMarker.setLatLng(userLocation);
 
-    // Post user's updated location to Firebase
-    postUserLocation(position);
-
+    // Перевірка наближення до якихось точок, якщо потрібно
+    // checkProximity();
     console.log('Місцезнаходження оновлено:', userLocation);
-}
-
-function listenForOtherUsers() {
-    database.ref('users').on('value', (snapshot) => {
-        const users = snapshot.val();
-        for (const userId in users) {
-            if (userId !== "user1") { // Exclude current user
-                const { latitude, longitude } = users[userId];
-                const userLocation = [latitude, longitude];
-
-                if (!otherUsersMarkers[userId]) {
-                    const otherUserIcon = L.divIcon({
-                        html: `<div class="icon-container">
-                                  <img src="otherUser.jpg" width="35" height="35">
-                               </div>`,
-                        iconSize: [39, 39],
-                        iconAnchor: [27, 27],
-                        popupAnchor: [-8, -27]
-                    });
-
-                    const marker = L.marker(userLocation, { icon: otherUserIcon }).addTo(map);
-                    otherUsersMarkers[userId] = marker;
-                } else {
-                    otherUsersMarkers[userId].setLatLng(userLocation);
-                }
-            }
-        }
-    });
 }
 
 function handleGeolocationError(error) {
     console.log("Не вдалося отримати геопозицію: " + error.message);
 }
 
+// Отримання початкового місцезнаходження та ініціалізація карти
 navigator.geolocation.getCurrentPosition(initializeMap, handleGeolocationError, {
     enableHighAccuracy: true,
     timeout: 5000
